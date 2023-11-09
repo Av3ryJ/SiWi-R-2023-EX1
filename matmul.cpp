93a79f2d95f9d8d484d1ce85ec4dfa6ed8fee4e4
Slimmeeyy <manuelha03@gmail.com>
2023-11-09 21:31:48 +0100

    naive implementierung in funktion ausgelagert

--- matmul.cpp
+++ matmul.cpp
@@ -6,52 +6,89 @@
 // Matrix A m*k, Matrix B k*n --> Matrix C m*n
 
 int main(int argc, char* argv[]){
-int m;
-int k;
-int n;
+    int m;
+    int k;
+    int n;
 
-if (argc < 4) {
-    std::cout << "Usage: matmul <mat1.in> <mat2.in> <output.out>" << std::endl;
-return -1;
+    if (argc < 4) {
+        std::cout << "Usage: matmul <mat1.in> <mat2.in> <output.out>" << std::endl;
+        return -1;
+    }
+
+    //MatA
+    std::string temp;
+    //Daten aus Dateien einlesen
+    std::ifstream fileA (argv[1]);
+    fileA>> m;
+    fileA>> k;
+
+    double *MatA = new double[m*k];
+
+    for (int i=0; i<(m*k); i++) {
+        fileA>>MatA[i];
+    }
+
+    //MatB
+    std::ifstream fileB(argv[2]);
+    fileB>> k;
+    fileB>> n;
+
+    double *MatB = new double[k*n];
+
+    for(int i=0; i<(k*n); i++){
+        fileB>>MatB[i];
+    }
+
+    //set outfile
+    std::string outfile = argv[3];
+    //MatC and time
+    double *MatC = new double[m*n];
+    double time = 100.0;
+
+    #ifdef USE_LIKWID
+        likwid_markerInit();
+        likwid_markerStartRegion( "array" );
+    #endif
+
+    siwir::Timer timer;
+
+    //naive implementation
+    int limit = 1;              // TODO: change back to 1000
+    for( int x = 0; x < limit; ++x ) {
+        timer.reset();
+        naive(MatA, MatB, MatC, m, k, n);
+        time = std::min(time, timer.elapsed());
+        if (x != limit - 1) {
+            delete [] MatC;
+        }
+    }
+
+    std::cout << "naive algorithm time: " << time << std::endl;
+
+    #ifdef USE_LIKWID
+        likwid_markerStopRegion( "array" );
+    #endif
+
+    std::cout << "time: " << time << std::endl;
+
+    std::ofstream fileO (outfile);
+    fileO << m << " " << n << std::endl;
+    for(int i = 0; i<(m*n); ++i) {
+        fileO << MatC[i] << "\n";
+    }
+    fileO.close();
+    delete [] MatA;
+    delete [] MatB;
+    delete [] MatC;
 }
-//MatA
-std::string temp;
-//Daten aus Dateien einlesen
-std::ifstream fileA (argv[1]);
-fileA>> m;
-fileA>> k;
 
-double *MatA = new double[m*k];
+/*
+matrix Mul:
+c11 = a11*b11 + a12*b21
+c11
+*/
 
-for (int i=0; i<(m*k); i++) {
-    fileA>>MatA[i];
-}
-//MatB
-std::ifstream fileB(argv[2]);
-fileB>> k;
-fileB>> n;
-
-double *MatB = new double[k*n];
-
-for(int i=0; i<(k*n); i++){
-    fileB>>MatB[i];
-}
-//set outfile
-std::string outfile = argv[3];
-//matC and time
-double *MatC = new double[m*n];
-double time = 100.0;
-
-#ifdef USE_LIKWID
-   likwid_markerInit();
-   likwid_markerStartRegion( "array" );
-#endif
-
-siwir::Timer timer;
-
-   for( int x = 0; x < 1; ++x ) // TODO: change back to 1000
-   {
-    timer.reset();
+void naive(double *MatA, double *MatB, double *MatC, int m, int k, int n) {
     // the next for is needed bc timer.h is too slow to track small multiplications
     for( int i = 0; i < 1; ++i ){ // TODO: change back to 10000
         for(int zeilenC=0; zeilenC<m; ++zeilenC){
@@ -61,28 +98,5 @@ siwir::Timer timer;
                 }
             }
         }
-      }
-    time = std::min(time, timer.elapsed());
-   }
-
-#ifdef USE_LIKWID
-   likwid_markerStopRegion( "array" );
-#endif
-std::cout << "time: " << time << std::endl;
-
-std::ofstream fileO (outfile);
-fileO << m << " " << n << std::endl;
-for(int i = 0; i<(m*n); ++i) {
-  fileO << MatC[i] << "\n";
+    }
 }
-fileO.close();
-delete [] MatA;
-delete [] MatB;
-delete [] MatC;
-}
-
-/*
-matrix Mul:
-c11 = a11*b11 + a12*b21
-c11
-*/
